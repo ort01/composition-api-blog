82703bac9e25e70f7246b208056e0acab1cbd8f0
ort01 <ortutova.kristina@gmail.com>
2023-01-12 21:06:58 +0100

    template refs in composition API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="home">hello</div>
+  <p>My name is {{ name }} and my age is {{ age }}</p>
+  <button ref="buttonRef" @click="handleClick">Click me</button>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// We import the parts from vue that we need to use like refs.
+import { ref } from "@vue/reactivity"; // this is generally the way that we work with the composition API.
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+
+  setup() {
+    let name = "John";
+    let age = 30;
+
+    const buttonRef = ref(null); //null is initial value; Getting the DOM element through ref="buttonRef" -> buttonRef= ref(initial value)
+
+    const handleClick = () => {
+      console.log(buttonRef.value); //<button>Click me</button>
+      buttonRef.value.classList.add("active"); //<button class="active">Click me</button>
+      buttonRef.value.textContent = "Changed textConent"; //<button>Changed textConent</button>
+    };
+
+    return { name, age, handleClick, buttonRef };
+  },
+};
 </script>
+
+
+<style>
+.active {
+  background: rgb(55, 163, 196);
+}
+</style>
