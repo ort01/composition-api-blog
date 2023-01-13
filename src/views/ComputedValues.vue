30de64b0734f3f5b45192711b4c9c73170522618
ort01 <ortutova.kristina@gmail.com>
2023-01-13 18:40:13 +0100

    watch and watchEffect

--- src/views/ComputedValues.vue
+++ src/views/ComputedValues.vue
@@ -5,10 +5,12 @@
   <div v-for="name in matchingNames" :key="name">
     {{ name }}
   </div>
+  <button @click="handleClick">Click</button>
 </template>
 
 <script>
 import { ref, computed } from "@vue/reactivity";
+import { watch, watchEffect } from "@vue/runtime-core";
 
 export default {
   name: "ComputedValues",
@@ -25,11 +27,36 @@ export default {
       "Jack",
     ]);
 
+    //watch hook
+    const stopWatch = watch(search, () => {
+      console.log("watch funtion ran");
+    }); //watch(X - value that i want to watch, function that fires when the value of x changes)
+
+    //watch effect
+    const stopWatchEffect = watchEffect(() => {
+      console.log("watch effect function ran", search.value);
+    }); //It watches any dependencies of the function inside watchEffect, any value that we use inside it. When the value of smth thats inside the function changes, it will automatically run this function.
+
     const matchingNames = computed(() => {
+      //computed watches the changes of the properties inside the computed function
       return names.value.filter((name) => name.includes(search.value));
     });
 
-    return { names, search, matchingNames };
+    const handleClick = () => {
+      //stoping the watch and watch effect functions from watching.
+      stopWatch();
+      stopWatchEffect();
+    };
+
+    return {
+      names,
+      search,
+      matchingNames,
+      stopWatch,
+      stopWatchEffect,
+      handleClick,
+      stopWatchEffect,
+    };
   },
 };
 </script>
